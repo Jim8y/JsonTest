9b7eb450e6d31f1a86325241f20503c434224db6
Liaojinghui <1498262377@qq.com>
2015-09-04 21:35:35 +0800

    Initial Commit

---
+++ JsonTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JsonTest
+//
+//  Created by 廖京辉 on 15/9/4.
+//  Copyright (c) 2015年 廖京辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
